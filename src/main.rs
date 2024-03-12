a10e18bf37531245c9cae1220c23702ca2e8f843
l3dlp <web@l3dlp.com>
2024-03-12 03:44:17 +0100

    English error messages

--- src/main.rs
+++ src/main.rs
@@ -16,31 +16,31 @@ fn main() {
     let image_path = Path::new(image_path);
 
     if !image_path.exists() {
-        eprintln!("Le fichier spécifié n'existe pas : {:?}", image_path);
+        eprintln!("No file: {:?}", image_path);
         std::process::exit(1);
     }
 
     let mut lt = match LepTess::new(None, "eng") {
         Ok(lt) => lt,
         Err(e) => {
-            eprintln!("Erreur lors de l'initialisation de Tesseract: {}", e);
+            eprintln!("Tesseract missing: {}", e);
             std::process::exit(1);
         }
     };
 
     if let Err(e) = lt.set_image(image_path) {
-        eprintln!("Erreur lors de la définition de l'image pour Tesseract: {}", e);
+        eprintln!("Tesseract error: {}", e);
         std::process::exit(1);
     }
 
     match lt.get_utf8_text() {
         Ok(text) => println!("{}", text),
-        Err(e) => eprintln!("Erreur lors de la reconnaissance du texte: {}", e),
+        Err(e) => eprintln!("No text found: {}", e),
     }
 
     match lt.get_utf8_text() {
         Ok(text) => regex::extract_information(&text),
-        Err(e) => eprintln!("Erreur lors de la reconnaissance des téléphones et emails : {}", e),
+        Err(e) => eprintln!("No text found: {}", e),
     }
 
 }
