b9ff04fb0e915b6c1bc370521049b872aa108f66
l3dlp <web@l3dlp.com>
2024-03-12 03:49:45 +0100

    English error messages

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rust-ocr
 A simple CLI OCR written in Rust
+
+### Usage
+
+CLI command only gets one argument, the file name:
+
+```bash
+./ocr <filename>
+```
+
+The program returns the detected test and the detected elements like emails.
--- src/regex.rs
+++ src/regex.rs
@@ -3,10 +3,14 @@ use regex::Regex;
 pub fn extract_information(text: &str) {
 
     let email_regex = Regex::new(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b").unwrap();
-    println!("\nEmails trouvés:");
-    for cap in email_regex.captures_iter(text) {
-        println!("{}", &cap[0]);
-    }
+	let email_match: Vec<_> = email_regex.captures_iter(text).collect(); 
+
+	if !email_match.is_empty() {
+		println!("\nEmails trouvés:");
+		for cap in email_regex.captures_iter(text) {
+			println!("{}", &cap[0]);
+		}
+	}
 
     // TODO: Enhance phone numbers matching regex
     let phone_regex = Regex::new(r"\b\d{10}\b").unwrap();
