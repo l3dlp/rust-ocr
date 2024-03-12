9e93ea36c9e98d788818e75b17da21c831de378e
l3dlp <web@l3dlp.com>
2024-03-12 03:33:40 +0100

    Add Regex extractor for email

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use leptess::LepTess;
 use std::env;
 use std::path::Path;
+use regex::Regex;
 
 fn main() {
+
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("Usage: {} <image_path>", args[0]);
@@ -34,4 +36,28 @@ fn main() {
         Ok(text) => println!("{}", text),
         Err(e) => eprintln!("Erreur lors de la reconnaissance du texte: {}", e),
     }
+
+    match lt.get_utf8_text() {
+        Ok(text) => extract_information(&text),
+        Err(e) => eprintln!("Erreur lors de la reconnaissance des téléphones et emails : {}", e),
+    }
+
+}
+
+fn extract_information(text: &str) {
+
+    let email_regex = Regex::new(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b").unwrap();
+    println!("\nEmails trouvés:");
+    for cap in email_regex.captures_iter(text) {
+        println!("{}", &cap[0]);
+    }
+
+    // TODO: Enhance phone numbers matching regex
+    let phone_regex = Regex::new(r"\b\d{10}\b").unwrap();
+    println!("\nNuméros de téléphone trouvés:");
+    for cap in phone_regex.captures_iter(text) {
+        println!("{}", &cap[0]);
+    }
+
+    println!("")
 }
